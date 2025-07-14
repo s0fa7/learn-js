253e898d03e433a420977b8561e040824a882625
eshkere <sofiafedotova2007@gmail.com>
2025-07-14 13:29:44 +0400

    learn objects

---
+++ objects.js
@@ -0,0 +1,38 @@
+let user = {
+    name: 'Sara',
+    age: 15
+};
+console.log(user.name);
+user['like dogs'] = true;
+delete user.age;
+
+for(let key in user){
+    console.log(user[key]);
+}
+
+function checkEmpty(obj){
+    for (let key in obj){
+        return false;
+    }
+    return true;
+}
+
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+}
+let zp = 0;
+// function sum(key){
+//     zp = zp + key;
+// }
+// function calculatingSalaries(obj){
+//     for (let key in obj){
+//         salaries[key].sum;
+//     }
+//     return zp;
+// }
+for (let key in salaries){
+        zp += salaries[key];
+     }
+console.log(`Общая зп сотрудников составляет ${zp}`)
